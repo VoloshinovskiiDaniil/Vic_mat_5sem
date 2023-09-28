7cfc6e34c800988cd8846250d5bb81e6c85af3de
79115 <79115@HUAWEI29>
2023-09-28 22:36:20 +0300

    Добавьте файлы проекта.

---
+++ main/functions.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include<iostream>
+#include<array>
+#include<eigen-3.4.0\Eigen\Dense>
+
+using namespace std;
+
+template<typename RealType, unsigned int N>
+struct DerivativeCoef
+{
+	RealType centralCoef;
+	array<RealType, N> otherCoeffs;
+};
+
+template<typename RealType, unsigned int N>
+DerivativeCoef<RealType, N>
+calcDerivativeCoef(const array<RealType, N>& points) noexcept
+{
+	Eigen::Matrix<RealType, N + 1, N + 1> A = Eigen::Matrix<RealType, N + 1, N + 1>::Ones();
+	for (unsigned int i = 1; i <= N; i++)
+	{
+		A(i, 0) = 0; // нулевой столбец заполнен нулями (кроме нулевой строки) - соответствует точке x0
+		for (unsigned int j = 1; j <= N; j++)
+			A(i, j) = A(i - 1, j) * points[j - 1] / i; // элементы матрицы напоминают коэффициенты разложения экспоненты в ряд Тейлора
+	}
+
+	Eigen::Vector<RealType, N + 1> b = Eigen::Vector<RealType, N + 1>::Zero();
+	b(1) = 1; //  почти нулевой столбец b системы A*x=b
+
+	Eigen::Vector<RealType, N + 1> x = A.colPivHouseholderQr().solve(b);
+
+	RealType centralCoef = x(0);
+
+	array<RealType, N> coefs;
+	for (unsigned int i = 0; i < N; i++)
+		coefs[i] = x(i + 1);
+
+	return DerivativeCoef<RealType, N>{centralCoef, coefs};
+}
+
+template<typename RealType, unsigned int N>
+RealType dif(const RealType x0, const RealType h, const array<RealType, N>& points)
+{
+	DerivativeCoef<double, N> coefs = calcDerivativeCoef<RealType, N>(points);
+
+	RealType dif_x0 = coefs.centralCoef * exp(x0) / h;
+	for (unsigned int i = 0; i < N; i++)
+		dif_x0 += coefs.otherCoeffs[i] * exp(x0 + points[i] * h) / h;
+
+	return dif_x0;
+}
---
+++ main/main.cpp
@@ -0,0 +1,55 @@
+﻿#include<functions.h>
+#include<fstream>
+
+using namespace std;
+
+
+
+int main()
+{
+	const double x0 = 1;
+	const array< double, 16> step = { 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 1e-14, 1e-15 };
+
+	ofstream check("check.txt");
+	check.precision(15);
+
+	for (int i = 0; i < 16; i++)
+		check << step[i] << "	" << dif<double, 2>(x0, step[i], { -1, 1 }) << endl;
+
+	check.close();
+
+	double graf_step[1000];
+	graf_step[0] = -15 * log(10);
+
+	for (int i = 1; i < 1000; i++)
+	{
+		double delta = abs(graf_step[0]) / 1000;
+		graf_step[i] = graf_step[i - 1] + delta;
+	}
+
+	ofstream data3("data3.txt");
+	data3.precision(15);
+
+	for (double x : graf_step)
+		data3 << x << " " << log(abs(dif<double, 3>(x0, exp(x), { -1, 1, 2 }) - exp(x0))) << endl;
+
+	data3.close();
+
+	ofstream data4("data4.txt");
+	data4.precision(15);
+
+	for (double x : graf_step)
+		data4 << x << " " << log(abs(dif<double, 4>(x0, exp(x), { -2, -1, 1, 2 }) - exp(x0))) << endl;
+
+	data4.close();
+
+	ofstream data5("data5.txt");
+	data5.precision(15);
+
+	for (double x : graf_step)
+		data5 << x << " " << log(abs(dif<double, 5>(x0, exp(x), { -2, -1, 1, 2, 3 }) - exp(x0))) << endl;
+
+	data5.close();
+
+	return 0;
+}
